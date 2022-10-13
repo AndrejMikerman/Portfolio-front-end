d74449e282ca8a97ba3ef5db604bf7648696b9e5
AndrejMikerman <111794283+AndrejMikerman@users.noreply.github.com>
2022-10-13 22:26:34 +0200

    Navigation bar and media query

--- Project/script.js
+++ Project/script.js
@@ -3,16 +3,18 @@
 //Dom Elements
 const buttonContainer = document.querySelector(".button-container");
 const fotos = document.querySelectorAll(".my-foto");
+const nav = document.querySelector("nav");
+const navArrow = document.querySelector(".down-arrow");
 
-const showpicture = function (e) {
+navArrow.addEventListener("click", function () {
+  nav.scrollIntoView({ behavior: "smooth" });
+});
+
+buttonContainer.addEventListener("click", function (e) {
   if (!e.target.classList.contains("btn")) return;
-  const fotoId = e.target.id;
-  fotos.forEach((foto) => foto.classList.add("hidden"));
-  document.querySelector(`.foto-${fotoId}`).classList.remove("hidden");
-};
-const showdefault = function () {
-  fotos.forEach((foto) => foto.classList.add("hidden"));
-  document.querySelector(`.foto-default`).classList.remove("hidden");
-};
-buttonContainer.addEventListener("mouseover", showpicture);
-buttonContainer.addEventListener("mouseout", showdefault);
+  const elementId = e.target.dataset.goto;
+  document.getElementById(elementId).scrollIntoView({ behavior: "smooth" });
+});
+// to DO
+// Click on arrow scroll to the nav
+// lazy loading
