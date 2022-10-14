9830d256b0dbbd4e753c318d13cd8e6ccb34b5ad
AndrejMikerman <111794283+AndrejMikerman@users.noreply.github.com>
2022-10-14 15:18:00 +0200

    Footer done and minor fixes

--- Project/script.js
+++ Project/script.js
@@ -5,16 +5,32 @@ const buttonContainer = document.querySelector(".button-container");
 const fotos = document.querySelectorAll(".my-foto");
 const nav = document.querySelector("nav");
 const navArrow = document.querySelector(".down-arrow");
+const backToTop = document.querySelector(".back-to-top-arrow");
+const header = document.querySelector("header");
+const copyright = document.querySelector(".copyright");
 
+//arrow to the nav menu
 navArrow.addEventListener("click", function () {
   nav.scrollIntoView({ behavior: "smooth" });
 });
 
+//nav menu scrolling
 buttonContainer.addEventListener("click", function (e) {
   if (!e.target.classList.contains("btn")) return;
   const elementId = e.target.dataset.goto;
   document.getElementById(elementId).scrollIntoView({ behavior: "smooth" });
 });
+
+//back to the top scroll
+backToTop.addEventListener("click", function () {
+  header.scrollIntoView({ behavior: "smooth" });
+});
+
+//Set Year in the Copyright
+const currentYear = new Date().getFullYear();
+copyright.textContent = `© ${
+  currentYear === 2022 ? "" : "2022-"
+}${currentYear} Andrii Ivasiuk 🙂`;
+
 // to DO
-// Click on arrow scroll to the nav
 // lazy loading
